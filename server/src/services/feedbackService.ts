af7810eebbdddfbb347fa1ec421009b9a2490e14
Harsh Kumar <harsh.kumar@intimetec.com>
2024-06-27 20:32:19 +0530

    created the trigger for updating foodItemAudit when new record is inserted into feedback table

--- server/src/services/feedbackService.ts
+++ server/src/services/feedbackService.ts
@@ -1,6 +1,7 @@
 import UserDetail from "../User/userDetail";
 import pool from "../config/dbConnection";
 import { GenericRepository } from "../repository/genericRepository";
+import SentimentService from "./sentimentService";
 
 class FeedbackService {
 	async provideFeedback(feedback: any): Promise<any> {
@@ -8,6 +9,10 @@ class FeedbackService {
 			const connection = await pool.getConnection();
 			feedback.userEmail = UserDetail.getUserDetail();
 			const genericRepository = new GenericRepository(pool, "Feedback");
+			const sentimentScore = await SentimentService.analyzeFeedbackSentiments(
+				feedback.comment
+			);
+			await connection.query(`SET @NEW_Sentiment = ?`, [sentimentScore]);
 			const [rows]: any = await genericRepository.add(feedback);
 			connection.release();
 			return rows.affectedRows ? rows : [];
--- server/src/services/sentimentService.ts
+++ server/src/services/sentimentService.ts
@@ -7,20 +7,9 @@ class SentimentAnalysisService {
 		this.sentiment = new Sentiment();
 	}
 
-	async analyzeFeedbackSentiments(
-		feedbacks: any[]
-	): Promise<{ feedback_id: number; sentiment: number }[]> {
-		const sentimentResults = feedbacks.map((feedback) => {
-			const result = this.sentiment.analyze(feedback.comment);
-			return {
-				feedback_id: feedback.feedback_id,
-				sentiment: result.score,
-				rating: feedback.rating,
-			};
-		});
-
-		return sentimentResults;
+	async analyzeFeedbackSentiments(feedbackComment: any): Promise<number> {
+		const result = this.sentiment.analyze(feedbackComment);
+		return result.score;
 	}
 }
-
-export const sentimentAnalysisService = new SentimentAnalysisService();
+export default new SentimentAnalysisService();
